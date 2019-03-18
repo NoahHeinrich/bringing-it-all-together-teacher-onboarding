4b02e03a51a4658d5cdc33c9fc87ce14b4a9c4aa
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-18 17:36:11 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  attr_reader :name, :breed, :id
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+end
