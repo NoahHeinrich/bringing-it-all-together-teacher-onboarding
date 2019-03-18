de1d92d5c9037ce2e13133b2cf0262860800ac8e
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-18 17:37:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   end
   
   def self.create_table
-    sql = "CREATE TABLE IF NOT EXISTS dogs("
+    sql = "CREATE TABLE IF NOT EXISTS dogs(id INTEGER PRIMARY KEY, name S"
     DB[:conn].execute(sql)
   end
 end
