55e0104a10420889986fcef876eb78bf9e71ed3c
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-18 17:42:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,6 +19,6 @@ class Dog
     sql = "INSERT INTO dogs (name,breed)
     VALUES(?,?)"
     DB[:conn].execute(sql,self.name,self.breed)
-    result = DB[:conn].excecute
+    result = DB[:conn].excecute("SELECT ")
   end
 end
