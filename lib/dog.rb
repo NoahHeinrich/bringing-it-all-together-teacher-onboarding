f8e8be9930750811d7190c06d3a20420fc96a640
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-18 17:59:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,5 +36,7 @@ class Dog
   end
   
   def self.find_or_create_by(name:,breed:)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    result = DB[:conn].execute(sql,name,breed)
   end
 end
